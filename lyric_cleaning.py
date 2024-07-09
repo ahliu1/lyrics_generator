fe562504034e0ec20790a815f7c1fcaa56a4840d
Amy Liu <al577@duke.edu>
2024-07-09 16:22:53 -0400

    debugged

--- lyric_cleaning.py
+++ lyric_cleaning.py
@@ -31,7 +31,8 @@ def clean_data(filename, artist_name):
     -------
     Bool:
     """
-    with open(filename, "r") as f:
+    #with open(filename, "r") as f:
+    with open(filename, "r", encoding="UTF-8", errors="ignore") as f:
         file = f.read()
     # Unnecessary Filler Words
     #removal = [artist_name, "Instrumental", " x ", " pt. ", " pt ", "1", "2", "3", "4", "5", "6", "7", "8", "9", "Spotify", "Pre-Chorus", "Post-Chorus", "Variation", "Title: ", "Verse", "Chorus", "Bridge", "Outro", "Intro", "[ ]", "[]", "[", "]", ":", "Lyrics"] + artist_name.split(" ")
